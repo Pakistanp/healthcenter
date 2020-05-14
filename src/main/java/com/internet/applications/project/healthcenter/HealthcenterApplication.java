183994047480431d565516225d3b32856da4f652
Pakistanp <ppp16@vp.pl>
2020-05-14 20:42:22 +0200

    Init project

---
+++ src/main/java/com/internet/applications/project/healthcenter/HealthcenterApplication.java
@@ -0,0 +1,13 @@
+package com.internet.applications.project.healthcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internet/applications/project/healthcenter/HealthcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internet.applications.project.healthcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
